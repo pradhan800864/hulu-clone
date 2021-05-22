8a11b908d2f3e0e951ebbc31739c716676aec1de
VCRA\ppattepu <ppattepu@vocera.com>
2021-05-22 19:15:01 +0530

    initial commit

---
+++ hulu-clone/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div class='header'>
+            <h1>this is header</h1>
+        </div>
+    )
+}
+
+export default Header
