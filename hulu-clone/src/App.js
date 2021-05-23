81c464b65b723782b04b72948239978b58a0eee2
VCRA\ppattepu <ppattepu@vocera.com>
2021-05-23 08:06:00 +0530

    Added Nav and header

--- hulu-clone/src/App.js
+++ hulu-clone/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import Header from './Header'
+import Nav from './Nav'
+import Results from './Results'
+
+
 function App() {
   return (
     <div className="app">
         <Header />
+
+        <Nav />
+
+        <Results />
     </div>
   );
 }
---
+++ hulu-clone/src/Results.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Results.css'
+function Results() {
+    return (
+        <div className="results">
+            <h1>this is results</h1>
+        </div>
+    )
+}
+
+export default Results
